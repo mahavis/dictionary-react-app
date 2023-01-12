847776403f9276922fd28a157f95546f750ea308
mahavis <mpn1984@gmail.com>
2023-01-12 21:25:54 +0100

    added photos using different API from Pexels

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,13 +2,19 @@ import axios from "axios";
 import React, { useState } from "react";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   const [keyword, Setkeyword] = useState("");
   const [results, setResults] = useState(null);
+  const [photos, setPhotos] = useState(null);
 
   function handleSubmit(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -17,6 +23,15 @@ export default function Dictionary() {
     //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleSubmit);
+    let pexelsApiKey =
+      "563492ad6f9170000100000121f2d5eddd1947ed888bc63d3f3f8cee";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios
+      .get(pexelsApiUrl, {
+        headers: headers,
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -37,6 +52,7 @@ export default function Dictionary() {
         <div className="hint">for eg: sunrise, solo, travel, variety...</div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
